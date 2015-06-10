089b15ee2604e63d125e80d81aff5d7e5924d943
Ben Johnson <johnsonbben@gmail.com>
2015-06-10 16:34:08 -0700

    added logger

--- file-deleter.rb
+++ file-deleter.rb
@@ -1,8 +1,13 @@
-require 'fileutils'
+require "fileutils"
+require "logger"
+
+log = Logger.new "log.txt"
 
 lines = File.readlines "./shares.txt"
 shares = lines.map(&:strip).reject{|l| l[0] == "#"}
 
 shares.each do |share|
-  FileUtils.rm Dir.glob(share)
+  files = Dir.glob share
+  log.info "Going to delete: #{files}"
+  FileUtils.rm files
 end
